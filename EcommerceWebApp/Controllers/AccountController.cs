098a93ca2c85ff186afdaa783c5151003f062fc5
Raheel-Izaz <raheelizaz3@gmail.com>
2024-02-19 11:25:48 +0500

    Ecommerce Web App In ASP.Net 8

--- EcommerceWebApp/Controllers/AccountController.cs
+++ EcommerceWebApp/Controllers/AccountController.cs
@@ -20,18 +20,7 @@ namespace EcommerceWebApp.Controllers
         {
             var adminCred = _db.Admin.Where(u => u.Email == admin.Email).SingleOrDefault();
 
-            if (adminCred != null)
-            {
-                bool isValid = (adminCred.Email == admin.Email && adminCred.Password == admin.Password);
-                if (isValid)
-                {
-                   var ident
-                }
-                else
-                {
-                    TempData["LoginError"] = "Invalid User Name or Email";
-                }
-            }
+            
             TempData["LoginError"] = "Invalid User Name or Email";
             return View(admin);
         }
